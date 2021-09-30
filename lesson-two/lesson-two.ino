cf48812becb1563460cf5c445266902eb4b353ff
Ted Daisey <Ted.T.Daisey@nasa.gov>
2021-09-30 14:05:37 -0400

    Added lesson two

--- lesson-two/lesson-two.ino
+++ lesson-two/lesson-two.ino
@@ -1,21 +1,23 @@
+int PinNumber = 9;
+
 void setup() {
   // put your setup code here, to run once:
 
-  // set pin 9 to output mode
-  pinMode(9, OUTPUT);
+  // set pin 13 to output mode
+  pinMode(PinNumber, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // tell pin 9 to output a high signal
-  digitalWrite(9, HIGH);
+  // tell pin 13 to output a high signal
+  digitalWrite(PinNumber, HIGH);
 
   // delay for some number of milliseconds
   delay(1000);
 
   // tell pin 13 to output a low signal
-  digitalWrite(9, LOW);
+  digitalWrite(PinNumber, LOW);
 
   // delay for some number of milliseconds
   delay(1000);
