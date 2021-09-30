63863e25fceb7b4929a9331a208152fdd3df52e6
Ted Daisey <Ted.T.Daisey@nasa.gov>
2021-09-30 14:26:01 -0400

    Added variables to lesson two

--- lesson-two/lesson-two.ino
+++ lesson-two/lesson-two.ino
@@ -1,24 +1,26 @@
-int PinNumber = 9;
+int PinNumber = 9;  // Variable that holds the arduino pin number we are using 
+int OnTime = 1000;  // Variable that holds the LED on time in milliseconds 
+int OffTime = 1000; // Variable that holds the LED off time in milliseconds
 
 void setup() {
   // put your setup code here, to run once:
 
-  // set pin 13 to output mode
+  // set pin to output mode
   pinMode(PinNumber, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // tell pin 13 to output a high signal
+  // tell pin to output a high signal, this turn the LED ON
   digitalWrite(PinNumber, HIGH);
 
-  // delay for some number of milliseconds
-  delay(1000);
+  // delay for some number of milliseconds while the LED is ON
+  delay(OnTime);
 
-  // tell pin 13 to output a low signal
+  // tell pin to output a low signal, this turns the LED OFF
   digitalWrite(PinNumber, LOW);
 
-  // delay for some number of milliseconds
-  delay(1000);
+  // delay for some number of milliseconds while the LED is OFF
+  delay(OffTime);
 }
